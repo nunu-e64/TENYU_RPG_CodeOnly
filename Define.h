81053ea573916db629f1ec245802ae59bedaa060
nunu-e64 <nunu-e64@users.noreply.github.com>
2014-11-27 19:50:29 +0900

    WARNINGDX_DISABLE

--- Define.h
+++ Define.h
@@ -15,6 +15,7 @@
 //#define DEBUG_MODE	//プログラマデバッグ版 （他のメンバーに渡す時にはコメントアウト）
 	#ifndef DEBUG_MODE
 		#define DEBUGDX_DISABLE	//マクロの有効化/無効化
+		#define WARNINGDX_DISABLE	//マクロの有効化/無効化
 		#define CHECK_TIME_DISABLE	//マクロの有効化/無効化
 		#define MEMORY_CHECK_DISABLE //マクロの有効化/無効化
 	#endif
